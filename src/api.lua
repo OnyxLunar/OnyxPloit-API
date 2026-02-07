4b790bc4a73eb9ba777193b9be1fcceb355ce3de
OnyxLunar <259956410+OnyxLunar@users.noreply.github.com>
2026-02-07 16:22:59 +0100

    Update api.lua

--- src/api.lua
+++ src/api.lua
@@ -18,7 +18,7 @@ end
 function Library:LoadScript(...)
     local args = {...}
     for i, v in ipairs(args) do
-        print(string.format("[%s] Injecting Script: \"%s\"", self.Name, tostring(v)))
+        print(string.format("[%s] Injecting Script: %s", self.Name, tostring(v)))
         local script = string.format("%s", tosring(v))
         loadstring(game:HttpGet(script, true))()
     end
