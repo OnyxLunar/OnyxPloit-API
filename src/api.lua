315c887838f8fc09544668df88dc052b81568cad
OnyxLunar <259956410+OnyxLunar@users.noreply.github.com>
2026-02-07 19:04:39 +0100

    added speedhacks

--- src/api.lua
+++ src/api.lua
@@ -1,6 +1,21 @@
 local Library = {}
 Library.__index = Library
 
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local function applySpeed(character, DESIRED_SPEED)
+	local humanoid = character:WaitForChild("Humanoid")
+
+	humanoid.WalkSpeed = DESIRED_SPEED
+
+	humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+		if humanoid.WalkSpeed ~= DESIRED_SPEED then
+			humanoid.WalkSpeed = DESIRED_SPEED
+		end
+	end)
+end
+
 function Library.new(name)
     local self = setmetatable({}, Library)
     self.Name = name or "OnyxPloit"
@@ -20,6 +35,17 @@ function Library:LoadScript(...)
     for i, v in ipairs(args) do
         print(string.format("[%s] Injecting Script: %s", self.Name, tostring(v)))
         loadstring(game:HttpGet(tostring(v), true))()
+    end
+end
+
+function Library:ChangeSpeed(...)
+    local args = {...}
+    for i, v in ipairs(args) do
+        if player.Character then
+	        applySpeed(player.Character, tostring(v))
+        end
+
+        --player.CharacterAdded:Connect(applySpeed)
     end
 end
 
